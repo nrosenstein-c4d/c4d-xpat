278961bd9c019f3283ace809d4bfd232767ecd5e
Niklas Rosenstein <rosensteinniklas@googlemail.com>
2013-01-06 11:18:01 +0100

    Initial Commit

---
+++ res/c4d_symbols.h
@@ -0,0 +1,31 @@
+//*********************************************************************\
+// File name        : c4d_symbols.h
+// Description      : symbol definition file for the plugin 
+// Created at       : 
+// Created by       : Resource editor
+// Modified by      : 
+//*********************************************************************/
+// WARNING : Only edit this file, if you exactly know what you are doing.
+// WARNING : The comments are important, too.
+
+enum
+{
+  _FIRST_ELEMENT_      = 10000,
+  // Dialog definitions of DLG_OPTIONS start here
+  DLG_OPTIONS,
+  EDT_HSPACE,
+  EDT_VSPACE,
+  BTN_SAVE,
+  // Dialog definitions of DLG_OPTIONS end here
+
+
+  XPAT_COMMAND_OPENOPTIONSDIALOG,
+  XPAT_COMMAND_OPENOPTIONSDIALOG_HELP,
+  XPAT_COMMAND_ALIGNHORIZONTAL,
+  XPAT_COMMAND_ALIGNHORIZONTAL_HELP,
+  XPAT_COMMAND_ALIGNVERTICAL,
+  XPAT_COMMAND_ALIGNVERTICAL_HELP,
+
+// End of symbol definition
+  _DUMMY_ELEMENT_
+};
